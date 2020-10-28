a1450b2da9179911967ba293a9611fb11380d1d4
Mateus Batista <mateus999batista@gmail.com>
2020-10-28 16:29:27 -0300

    arquivos iniciais

---
+++ trabalho01/processarEntrada.c
@@ -0,0 +1,13 @@
+#include "montador.h"
+#include <stdio.h>
+
+/*
+    ---- Você Deve implementar esta função para a parte 1.  ----
+    Essa função é chamada pela main em main.c
+    Entrada da função: arquivo de texto lido e seu tamanho
+*/
+int processarEntrada(char* entrada, unsigned tamanho)
+{
+    /* printf("Você deve implementar esta função para a Parte 1.\n"); */
+    return 0;
+}
--- trabalho01/trab01.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-    char* str = "vetor1:\nvetor2:   .word 0x10 # (hexadecimal) Comentario apos diretiva\nvetor3:   .word 10 # (Decimal) Comentario apos diretiva\n.word 10\n.word 10  # Comentario apos diretiva\n# Comentario sozinho\n\n# vetor4: ADD 1";  
-
-    
-}
