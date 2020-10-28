fae9c61b6d36afde5708156cbcf2b128bb5f0b6a
Mateus Batista <mateus999batista@gmail.com>
2020-10-28 19:00:29 -0300

    tokenizaçao das palavras

--- trabalho01/processarEntrada.c
+++ trabalho01/processarEntrada.c
@@ -1,13 +1,43 @@
 #include "montador.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/*
-    ---- Você Deve implementar esta função para a parte 1.  ----
-    Essa função é chamada pela main em main.c
-    Entrada da função: arquivo de texto lido e seu tamanho
-*/
-int processarEntrada(char* entrada, unsigned tamanho)
-{
-    /* printf("Você deve implementar esta função para a Parte 1.\n"); */
+int processarEntrada(char* entrada, unsigned tamanho){       
+    char ** tokens = malloc(tamanho*sizeof(char*));
+    
+    for (int i = 0; i < tamanho; i++)
+    {
+        tokens[i] = malloc(50*sizeof(char));
+    }
+    
+
+    char * token = strtok(entrada, " ");
+    int t = 0;
+    while( token != NULL ) {
+      for (int i = 0; i < 50; i++){
+          tokens[t][i] = token[i];
+
+      }
+      t++;
+      
+      token = strtok(NULL, " ");
+    }
+
+    for (int i = 0; strlen(tokens[i]) > 0; i++)
+    {
+        printf(" %s\n",tokens[i]);
+    }
+    
+    
+    
     return 0;
+}
+
+int main(){
+    char string[1000] = "vetor1:\nvetor2:   .word 0x10 # (hexadecimal) Comentario apos diretiva\nvetor3:   .word 10 # (Decimal) Comentario apos diretiva\n.word 10\n.word 10  # Comentario apos diretiva\n# Comentario sozinho\n\n# vetor4: ADD 1";
+    int tamanho = 200;
+    
+
+    processarEntrada(string,100);
 }
