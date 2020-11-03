64480329ccfcc9901974483d797acc9d3822c3d5
Mateus Batista <mateus999batista@gmail.com>
2020-11-03 11:12:50 -0300

    pegando as linhas vazias

--- trabalho01/processarEntrada.c
+++ trabalho01/processarEntrada.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <token.h>
 
 /* Exemplo
 
@@ -25,33 +26,46 @@ vetor3:   .word 10 # (Decimal) Comentario apos diretiva
 
 
 int processarEntrada(char* entrada, unsigned tamanho){           
-    
 
     char ** lines = malloc(tamanho*sizeof(char*));
     for (int i = 0; i < tamanho; i++) lines[i] = malloc(200*sizeof(char)); //dar free
               
-    char * line = strtok(entrada, "\n");
-    int t = 0;
-    while( line != NULL ) {
+    // char * line = strtok(entrada, "\n");
+    // int t = 0;
+    // while( line != NULL ) {
      
-      for (int i = 0; i < 200; i++){ 
-        lines[t][i] = line[i];
-      }
-      t++;      
-      line = strtok(NULL, "\n");
-    }
-    // separação de linhas pronta
+    //   for (int i = 0; i < 200; i++){ 
+    //     lines[t][i] = line[i];
+    //   }
+    //   t++;      
+    //   line = strtok(NULL, "\n");
+    // }
+    int cont = 1;
+    int t = 0;
+    for (int i = 0; i < tamanho; i++){
+        char line[200] = { NULL };
+        int j = 0;
+        while (t < strlen(entrada) && entrada[t] != '\n'){
+            line[j] = entrada[t];
+            j++;
+            t++;
+            if(entrada[t] == '\n' || entrada[t] == '\0') cont++;
+        }
+        for (int k = 0; k < 200; k++){ 
+            lines[i][k] = line[k];
+        }
+        
+        t++;
     
-    
+    } 
+    printf("cont %d\n",cont);
+
     char ** tokens = malloc(tamanho*sizeof(char*));
     for (int i = 0; i < tamanho; i++) tokens[i] = malloc(50*sizeof(char)); // dar free
 
-        
-
     int k = 0;
-    t = 0;
-    while(strlen(lines[k]) > 0){
-        
+     t = 0;
+    while(k < cont){
         char *oldstr = malloc(sizeof(lines[k])); 
         strcpy(oldstr,lines[k]);
         
@@ -59,13 +73,13 @@ int processarEntrada(char* entrada, unsigned tamanho){
 
         if (token != NULL && strchr(token, '#')){
             token = NULL;
-           
         }
         
         while( token != NULL ) {
             for (int i = 0; i < 50; i++){ 
                 tokens[t][i] = token[i]; // criar objetos do tipo Token?
-            }            
+            }
+                        
             t++;     
             token = strtok(NULL, " ");
             if (token!= NULL && strchr(token, '#')){
@@ -78,23 +92,24 @@ int processarEntrada(char* entrada, unsigned tamanho){
     }
     
     
-         
-
-    for (int i = 0; strlen(lines[i]) > 0; i++){
-        printf("linha %d : %s\n",i,lines[i]);
-    } 
-    printf("\n");
     for (int i = 0; strlen(tokens[i]) > 0 ; i++){
+        
         printf("token %d : %s\n",i,tokens[i]);
     } 
+
+
+    for (int i = 0; i < cont; i++){
         
+        printf("linha %d : %s\n",i,lines[i]);
+       
+    }     
     
     return 0;
 }
 
 int main(){
     char string[1000] = "vetor1:\nvetor2:   .word 0x10 # (hexadecimal) Comentario apos diretiva\nvetor3:   .word 10 # (Decimal) Comentario apos diretiva\n.word 10\n.word 10  # Comentario apos diretiva\n# Comentario sozinho\n\n# vetor4: ADD 1";
-    int tamanho = 200;
+    int tamanho = 10;
     
     printf("%s\n\n",string);
 
